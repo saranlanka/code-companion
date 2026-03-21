d175c90e4f4ace2436a44a2a4603348afd835799
Lanka Saran Manikanta swamy <saranlanka033@gmail.com>
2026-03-21 22:46:35 +0530

    Add redirect handling and wrap App in StrictMode

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,16 @@
-import { createRoot } from "react-dom/client";
+const redirect = sessionStorage.redirect;
+delete sessionStorage.redirect;
+if (redirect && redirect !== location.href) {
+  history.replaceState(null, "", redirect);
+}
+
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
